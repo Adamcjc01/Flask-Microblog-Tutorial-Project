34cee23cdf8a3264547735407353352d69cd0485
Adamcjc01 <adamcjc01@gmail.com>
2020-07-01 16:33:31 +0100

    Added AJAX translation to posts and changed the name

---
+++ app/translate.py
@@ -0,0 +1,26 @@
+import json
+import requests
+import uuid
+import os
+from flask_babel import _
+from app import app
+
+def translate(text, dest_language):
+	if 'MS_TRANSLATOR_KEY' not in app.config or \
+			not app.config['MS_TRANSLATOR_KEY']:
+		return _('Error: the translation service is not configured.')
+	subscription_key = app.config['MS_TRANSLATOR_KEY']
+	endpoint = 'https://api.cognitive.microsofttranslator.com'
+	path = '/translate?api-version=3.0'
+	params = f'&to={dest_language}'
+	constructed_url = endpoint + path + params
+	headers = {
+		'Ocp-Apim-Subscription-Key': subscription_key,
+		"Ocp-Apim-Subscription-Region":'westeurope',
+		'Content-type': 'application/json',
+		'X-ClientTraceId': str(uuid.uuid4())
+		}
+	body = [{'text': text}]
+	request = requests.post(constructed_url, headers=headers, json=body)
+	response = request.json()
+	return response[0]['translations'][0]['text']
--- config.py
+++ config.py
@@ -13,3 +13,4 @@ class Config(object):
 	ADMINS = ['adamcjc01@gmail.com']
 	POSTS_PER_PAGE = 25
 	LANGUAGES = ['en', 'es']
+	MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
